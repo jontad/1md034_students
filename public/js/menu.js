a8b668386f69aff282b96a4b3a7729abe60a76ac
Jonathan <jonathannoahtadese@gmail.com>
2020-02-12 14:16:23 +0100

    forgot menu.js

---
+++ public/js/menu.js
@@ -0,0 +1,46 @@
+
+let food = [
+    {
+	"name": "Vegan burger",
+	"kCal": "850 kCal",
+	"lactose": "Contains gluten",
+	"gluten": "Contains Lactose",
+	"info": "Very Tasty",
+	"img": "https://www.thespruceeats.com/thmb/KAgMssHoQUmx30uuYL_FTahXA0A=/2048x1360/filters:fill(auto,1)/vegan-mushroom-bean-burger-recipe-3378623-13_preview1-5b241897fa6bcc0036d2c9bf.jpeg"
+    },
+    {
+	"name": "Hamburger",
+	"kCal": "850 kCal",
+	"lactose": "Contains gluten",
+	"gluten": "Contains Lactose",
+	"info": "Very Tasty",
+	"img": "https://www.burgerking.se/011_se/Product%20images/Burgers/image-thumb__7664__product_detail/KINGJR%20HAMBURGER%201500x1500.png"
+    },
+    {
+	"name": "Meatburger",
+	"kCal": "850 kCal",
+	"lactose": "Contains gluten",
+	"gluten": "Contains Lactose",
+	"info": "Very Tasty",
+	"img": "https://s3.ap-southeast-1.amazonaws.com/cdn.deccanchronicle.com/sites/default/files/lab-grown%20meat%20burger.jpg"
+    },
+    {
+	"name": "Fishburger",
+	"kCal": "850 kCal",
+	"lactose": "Contains gluten",
+	"gluten": "Contains Lactose",
+	"info": "Very Tasty",
+	"img": "https://st3.depositphotos.com/1044737/14720/i/1600/depositphotos_147201311-stock-photo-fish-burger-fishburger-hamburger-tomatoes.jpg"
+    },
+    {
+	"name": "Cheeseburger",
+	"kCal": "850 kCal",
+	"lactose": "Contains gluten",
+	"gluten": "Contains Lactose",
+	"info": "Very Tasty",
+	"img": "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/nutrition/items/hero/desktop/t-mcdonalds-Cheeseburger.jpg"
+    }
+]
+
+
+
